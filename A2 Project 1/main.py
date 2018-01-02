e390812c0b200cd13f369a4cead12bd182a3bc63
godlikeanyone <1093496273@qq.com>
2018-01-02 14:13:04 +0800

    Final main.py
    This is final work

--- A2 Project 1/main.py
+++ A2 Project 1/main.py
@@ -15,6 +15,162 @@ bg_size = width, height = 480, 960
 screen = pygame.display.set_mode(bg_size)
 pygame.display.set_caption("Thunder-The Last War")
 background = pygame.image.load("images/universe.png").convert()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+#Load the sound
+pygame.mixer.music.load("sound/game_music.ogg")
+pygame.mixer.music.set_volume(0.2)
+bullet_sound = pygame.mixer.Sound("sound/bullet.wav")
+bullet_sound.set_volume(0.2)
+bomb_sound = pygame.mixer.Sound("sound/use_bomb.wav")
+bomb_sound.set_volume(0.2)
+supply_sound = pygame.mixer.Sound("sound/supply.wav")
+supply_sound.set_volume(0.2)
+get_bomb_sound = pygame.mixer.Sound("sound/get_bomb.wav")
+get_bomb_sound.set_volume(0.2)
+get_bullet_sound = pygame.mixer.Sound("sound/get_bullet.wav")
+get_bullet_sound.set_volume(0.2)
+upgrade_sound = pygame.mixer.Sound("sound/upgrade.wav")
+upgrade_sound.set_volume(0.2)
+enemy3_fly_sound = pygame.mixer.Sound("sound/enemy3_flying.wav")
+enemy3_fly_sound.set_volume(0.2)
+enemy1_down_sound = pygame.mixer.Sound("sound/enemy1_down.wav")
+enemy1_down_sound.set_volume(0.2)
+enemy2_down_sound = pygame.mixer.Sound("sound/enemy2_down.wav")
+enemy2_down_sound.set_volume(0.2)
+enemy3_down_sound = pygame.mixer.Sound("sound/enemy3_down.wav")
+enemy3_down_sound.set_volume(0.5)
+me_down_sound = pygame.mixer.Sound("sound/me_down.wav")
+me_down_sound.set_volume(0.2)
+
+
+def add_small_enemies(group1, group2, num):
+    for i in range(num):
+        e1 = enemy.SmallEnemy(bg_size)
+        group1.add(e1)
+        group2.add(e1)
+
+def add_mid_enemies(group1, group2, num):
+    for i in range(num):
+        e2 = enemy.MidEnemy(bg_size)
+        group1.add(e2)
+        group2.add(e2)
+
+def add_big_enemies(group1, group2, num):
+    for i in range(num):
+        e3 = enemy.BigEnemy(bg_size)
+        group1.add(e3)
+        group2.add(e3)
+
+def inc_speed(target, inc):
+    for each in target:
+        each.speed += inc
+
+def main():
+    pygame.mixer.music.play(-1)
+
+    #Add my plane
+    me = myplane.MyPlane(bg_size)
+
+    enemies = pygame.sprite.Group()
+
+    #Create enemies
+    small_enemies = pygame.sprite.Group()
+    add_small_enemies(small_enemies, enemies, 15)
+
+    
+    mid_enemies = pygame.sprite.Group()
+    add_mid_enemies(mid_enemies, enemies, 4)
+
+    
+    big_enemies = pygame.sprite.Group()
+    add_big_enemies(big_enemies, enemies, 2)
+
+    #Create bullet
+    bullet1 = []
+    bullet1_index = 0
+    BULLET1_NUM = 4
+    for i in range(BULLET1_NUM):
+        bullet1.append(bullet.Bullet1(me.rect.midtop))
+
+    #Create super bullet
+    bullet2 = []
+    bullet2_index = 0
+    BULLET2_NUM = 8
+    for i in range(BULLET2_NUM//2):
+        bullet2.append(bullet.Bullet2((me.rect.centerx-33, me.rect.centery)))
+        bullet2.append(bullet.Bullet2((me.rect.centerx+30, me.rect.centery)))
+
+    clock = pygame.time.Clock()
+
+    #Get shoot by enemy
+    e1_destroy_index = 0
+    e2_destroy_index = 0
+    e3_destroy_index = 0
+    me_destroy_index = 0
+
+    #My score
+    score = 0
+    score_font = pygame.font.Font("font/font.ttf", 36)
+
+    #Pause game
+    paused = False
+    pause_nor_image = pygame.image.load("images/pause_nor.png").convert_alpha()
+    pause_pressed_image = pygame.image.load("images/pause_pressed.png").convert_alpha()
+    resume_nor_image = pygame.image.load("images/resume_nor.png").convert_alpha()
+    resume_pressed_image = pygame.image.load("images/resume_pressed.png").convert_alpha()
+    paused_rect = pause_nor_image.get_rect()
+    paused_rect.left, paused_rect.top = width - paused_rect.width - 10, 10
+    paused_image = pause_nor_image
+
+    #Level of difficulty
+    level = 1
+
+    #Super powerful bomb
+    bomb_image = pygame.image.load("images/bomb.png").convert_alpha()
+    bomb_rect = bomb_image.get_rect()
+    bomb_font = pygame.font.Font("font/font.ttf", 48)
+    bomb_num = 3
+
+    #Supply
+    bullet_supply = supply.Bullet_Supply(bg_size)
+    bomb_supply = supply.Bomb_Supply(bg_size)
+    SUPPLY_TIME = USEREVENT
+    pygame.time.set_timer(SUPPLY_TIME, 30 * 1000)
+
+    #Clock for super bullet
+    DOUBLE_BULLET_TIME = USEREVENT + 1
+
+    #Status of super bullet
+    is_double_bullet = False
+
+    #Clock for invincible condition of my plane
+    INVINCIBLE_TIME = USEREVENT + 2
+
+    #Number of lives
+    life_image = pygame.image.load("images/life.png").convert_alpha()
+    life_rect = life_image.get_rect()
+    life_num = 3
+
+    #Prevent open record file again and again
+    recorded = False
+
+    #End of game
+    gameover_font = pygame.font.Font("font/font.TTF", 48)
+    again_image = pygame.image.load("images/again.png").convert_alpha()
+    again_rect = again_image.get_rect()
+    gameover_image = pygame.image.load("images/gameover.png").convert_alpha()
+    gameover_rect = gameover_image.get_rect()
+
+    #Switch image
+    switch_image = True
+
+    #Time delay
+    delay = 100
+
 
 def main():
     clock = pygame.time.Clock()
